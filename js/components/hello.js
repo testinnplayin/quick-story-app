491071c9146a5645ceed7480cb375fdee693156e
R Wood <archaurore@gmail.com>
2017-02-05 10:23:42 -0500

    created a hello test component

---
+++ js/components/hello.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+
+export default function Hello() {
+	return (
+		<div className="hello"><p>Hello World!</p></div>
+	);
+};
--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './components/hello';
+
+document.addEventListener('DOMContentLoaded', () => {
+	ReactDOM.render(<Hello />, document.getElementById('app'));
+});

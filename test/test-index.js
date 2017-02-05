a343ca7cac0050683fc90aabe9db3ed08bd05b88
R Wood <archaurore@gmail.com>
2017-02-05 15:56:44 -0500

    added unit test for main container component

--- test/test-index.js
+++ test/test-index.js
@@ -6,20 +6,38 @@ import chai from 'chai';
 
 const should = chai.should();
 
-import Hello from '../js/components/hello';
+import MainContainer from '../js/components/main-container';
 
-describe('Hello component', function() {
-	it('should render a greeting', function() {
-		let str = 'Hello World!';
+describe('MainContainer component', function() {
+	it('should render the overarching container', function() {
+		
 
 		const renderer = TestUtils.createRenderer();
-		renderer.render(<Hello />);
+		renderer.render(<MainContainer />);
 
 		const result = renderer.getRenderOutput();
-		result.props.className.should.equal('hello');
+		result.props.className.should.equal('mainContainer');
 
-		let p = result.props.children;
+		const main = result.props.children[1];
+		main.type.should.equal('main');
+
+		const section = main.props.children[0]
+		section.props.className.should.equal('introductory-section');
+		section.type.should.equal('section');
+
+		const h1 = section.props.children[0];
+		h1.type.should.equal('h1');
+		h1.props.children.should.equal('Quick Story!');
+
+		const span = section.props.children[1];
+		span.type.should.equal('span');
+		
+		const strong = span.props.children;
+		strong.type.should.equal('strong');
+		strong.props.children.should.be.a('string');
+		strong.props.children.should.equal('The random photo story app');
+
+		const p = section.props.children[2];
 		p.type.should.equal('p');
-		p.props.children.should.equal(str);
 	});
 });

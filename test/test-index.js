2fd56086a2874fb78cdb2cbd77118487a8449a64
R Wood <archaurore@gmail.com>
2017-02-10 09:46:44 -0500

    initial commit with new idea for structuring app

--- test/test-index.js
+++ test/test-index.js
@@ -6,38 +6,3 @@ import chai from 'chai';
 
 const should = chai.should();
 
-import {MainContainer} from '../js/components/main-container';
-
-describe('MainContainer component', function() {
-	it('should render the overarching container', function() {
-		
-
-		const renderer = TestUtils.createRenderer();
-		renderer.render(<MainContainer />);
-
-		const result = renderer.getRenderOutput();
-		result.props.className.should.equal('mainContainer');
-
-		const main = result.props.children[1];
-		main.type.should.equal('main');
-
-		const section = main.props.children[0]
-		section.props.className.should.equal('introductory-section');
-		section.type.should.equal('section');
-
-		const h1 = section.props.children[0];
-		h1.type.should.equal('h1');
-		h1.props.children.should.equal('Quick Story!');
-
-		const span = section.props.children[1];
-		span.type.should.equal('span');
-		
-		const strong = span.props.children;
-		strong.type.should.equal('strong');
-		strong.props.children.should.be.a('string');
-		strong.props.children.should.equal('The random photo story app');
-
-		const p = section.props.children[2];
-		p.type.should.equal('p');
-	});
-});

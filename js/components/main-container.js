ec0475b5220f1bdd6333fb1229a6ff090ab16e80
R Wood <archaurore@gmail.com>
2017-02-10 10:45:14 -0500

    created a main container and its test

---
+++ js/components/main-container.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+import {connect} from 'react-redux';
+
+import NavBar from './navbar';
+
+export class MainContainer extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="mainContainer">
+				<NavBar />
+				<main>
+					
+				</main>
+			</div>
+		);
+	}
+};
---
+++ test/test-main-container.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import chai from 'chai';
+
+const should = chai.should();
+
+import {MainContainer} from '../js/components/main-container';
+
+describe('MainContainer component', function() {
+	it('should render an element that contains a navbar', function() {
+		const renderer = TestUtils.createRenderer();
+		renderer.render(<MainContainer />);
+
+		const result = renderer.getRenderOutput();
+		result.type.should.equal('div');
+		result.props.className.should.equal('mainContainer');
+
+		let main = result.props.children;
+		main.type.should.equal('main');
+
+		// let section = main.props.children;
+		// section.type.should.equal('section');
+		// section.props.className.should.equal('main-title');
+
+		// let instructionArr = section.props.children;
+		// instructionArr.should.be.a('array');
+		// instructionArr.should.have.lengthOf(2);
+
+		// let h1 = instructionArr[0];
+		// h1.type.should.equal('h1');
+		// h1.props.children.should.be.a('string');
+
+		// let p = instructionArr[1];
+		// p.type.should.equal('p');
+		// p.props.children.should.be.a('string');
+	});
+});

47044d7cd74ff045f1dac41232601f4c98967866
R Wood <archaurore@gmail.com>
2017-02-15 11:42:37 -0500

    added more fields to the text area form

--- js/components/text-area.js
+++ js/components/text-area.js
@@ -11,8 +11,12 @@ export class TextArea extends React.Component {
 	render() {
 		return (
 			<form className="textArea">
+				<label htmlFor="title-input">Title:</label>
+				<input type="text" id="title-input" name="title-input" />
 				<label htmlFor="story-area">Write story below:</label>
 				<input type="textarea" id="story-area" name="story-area" />
+				<label htmlFor="author-input">By:</label>
+				<input type="text" id="author-input" name="author-input" />
 				<button type="submit">{this.props.submitBtnName}</button>
 			</form>
 		);
--- test/test-text-area.js
+++ test/test-text-area.js
@@ -9,7 +9,7 @@ const should = chai.should();
 import {TextArea} from '../js/components/text-area';
 
 describe('TextArea component', function() {
-	it('should render a form with a label, text area and button', function() {
+	it('should render a form with labels, two text inputs, one text area input and a button', function() {
 		const renderer = TestUtils.createRenderer();
 		renderer.render(<TextArea />);
 
@@ -19,16 +19,39 @@ describe('TextArea component', function() {
 
 		const formArr = result.props.children;
 		formArr.should.be.a('array');
-		formArr.should.have.lengthOf(3);
+		formArr.should.have.lengthOf(7);
 
-		const label = formArr[0];
-		label.type.should.equal('label');
+		const label1 = formArr[0];
+		label1.type.should.equal('label');
+		label1.props.htmlFor.should.equal('title-input')
+		label1.props.children.should.equal('Title:');
 
-		const input = formArr[1];
-		input.type.should.equal('input');
-		input.props.type.should.equal('textarea');
+		const input1 = formArr[1];
+		input1.type.should.equal('input');
+		input1.props.type.should.equal('text');
+		input1.props.id.should.equal('title-input');
 
-		const button = formArr[2];
+		const label2 = formArr[2];
+		label2.type.should.equal('label');
+		label2.props.htmlFor.should.equal('story-area');
+		label2.props.children.should.equal('Write story below:');
+
+		const input2 = formArr[3];
+		input2.type.should.equal('input');
+		input2.props.type.should.equal('textarea');
+		input2.props.id.should.equal('story-area');
+
+		const label3 = formArr[4];
+		label3.type.should.equal('label');
+		label3.props.htmlFor.should.equal('author-input');
+		label3.props.children.should.equal('By:')
+
+		const input3 = formArr[5];
+		input3.type.should.equal('input');
+		input3.props.type.should.equal('text');
+		input3.props.id.should.equal('author-input');
+
+		const button = formArr[6];
 		button.type.should.equal('button');
 		button.props.type.should.equal('submit');
 

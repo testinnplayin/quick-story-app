a3060c3bc82ad8cc6944e520ecfa65dd2576aa94
R Wood <archaurore@gmail.com>
2017-02-16 05:48:13 -0500

    created a first draft of story schema model

--- models.js
+++ models.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+const storySchema = mongoose.Schema({
+	title : { type : String, required : true },
+	photo : { type : String, required : true },
+	story : String,
+	author : {
+		firstName : String,
+		lastName : String
+	}
+});
+
+storySchema.virtual('nameString').get(function() {
+	return `${this.author.lastName}, ${this.author.firstName}`;
+});
+
+storySchema.methods.apiRepr = function() {
+	return {
+		id : this._id,
+		title : this.title,
+		photo : this.photo,
+		story : this.story,
+		author : this.nameString
+	}
+};
+
+const Story = mongoose.model('Story', storySchema);
+
+module.exports = {Story};

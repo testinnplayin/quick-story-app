b0733dbcc71281300ab5e3e93c8cf21836e5fe55
R Wood <archaurore@gmail.com>
2017-04-28 10:50:09 +0200

    checked for props being passed

--- test/test-general-container.js
+++ test/test-general-container.js
@@ -1,7 +1,7 @@
 'use strict';
 
+import {shallow} from 'enzyme';
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
 import chai from 'chai';
 
 const should = chai.should();
@@ -10,23 +10,41 @@ import {GeneralContainer} from '../js/containers/general-container';
 
 describe('GeneralContainer component', function() {
 	it('should draw a component that contains a main with a section', function() {
-		const renderer = TestUtils.createRenderer();
-		renderer.render(<GeneralContainer />);
+		const fakeProps = {
+			title : 'Amazing Story',
+			buttonNames : ['Edit', 'Write'],
+			photoArea : 'www.example.com/photo.jpg',
+			photoBtn : 'Get Photo'
+		},
+			wrapper = shallow(
+				<GeneralContainer
+					title={fakeProps.title}
+					buttonNames={fakeProps.buttonNames}
+					photoArea={fakeProps.photoArea}
+					photoBtn={fakeProps.photoBtn} />
+			);
 
-		const result = renderer.getRenderOutput();
-		result.type.should.equal('main');
-		result.props.className.should.equal('generalContainer container-fluid');
+		const main = wrapper.find('main');
+		main.hasClass('generalContainer container-fluid');
 
-		const mainArr = result.props.children;
+		const mainArr = main.node.props.children;
 		mainArr.should.be.a('array');
 		mainArr.should.have.lengthOf(2);
 
-		const section = mainArr[1];
-		section.type.should.equal('section');
-		section.props.className.should.equal('generalArea');
+		const mainTitle = mainArr[0];
+		mainTitle.props.title.should.equal(fakeProps.title);
 
-		const sectionArr = section.props.children;
+		const section = wrapper.find('.generalArea');
+		section.hasClass('generalArea');
+
+		const sectionArr = section.node.props.children;
 		sectionArr.should.be.a('array');
 		sectionArr.should.have.lengthOf(2);
+
+		const initPhotoAreaContainer = sectionArr[0];
+		initPhotoAreaContainer.props.photoArea.should.equal(fakeProps.photoArea);
+
+		const initTextAreaContainer = sectionArr[1];
+		initTextAreaContainer.props.photoBtn.should.equal(fakeProps.photoBtn);
 	});
 });

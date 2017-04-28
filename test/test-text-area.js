cf2d467f9ace6e5a3a0c4e4993067f4baaebefa2
R Wood <archaurore@gmail.com>
2017-04-28 10:35:42 +0200

    added test for submit event on story form in textArea

--- test/test-text-area.js
+++ test/test-text-area.js
@@ -1,7 +1,7 @@
 'use strict';
 
+import {shallow} from 'enzyme';
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
@@ -10,39 +10,46 @@ const should = chai.should();
 chai.use(sinonChai);
 
 import {TextArea} from '../js/components/text-area';
+import * as actions from '../js/actions/index';
 
 describe('TextArea component', function() {
 	it('should render a form with divs, labels and a button', function() {
-		const renderer = TestUtils.createRenderer();
-		renderer.render(<TextArea />);
+		const fakeProps = {
+			submitBtnName : 'Save'
+		},
+			wrapper = shallow(<TextArea submitBtnName={fakeProps.submitBtnName} />),
+			form = wrapper.find('form');
 
-		const result = renderer.getRenderOutput();
-		result.type.should.equal('form');
-		result.props.className.should.equal('textArea');
-		result.props.onSubmit.should.be.a('function');
+		form.node.type.should.equal('form');
+		form.hasClass('textArea');
 
-		const formArr = result.props.children;
+		const formArr = form.node.props.children;
 		formArr.should.be.a('array');
 		formArr.should.have.lengthOf(4);
 
-		let lng = formArr.length;
-
-		for (let i = 0; i < lng -1; i++) {
-			let div = formArr[i];
-
-			div.type.should.equal('div');
-			div.props.className.should.equal('form-group');
-
-			let divArr = div.props.children;
-			divArr.should.be.a('array');
-			divArr.should.have.lengthOf(2);
-			divArr[0].type.should.equal('label');
-
-		}
-
 		const button = formArr[3];
-		button.type.should.equal('button');
-		button.props.type.should.equal('submit');
+		button.props.children.should.equal(fakeProps.submitBtnName);
+	});
 
+	it('should have a submit event', function() {
+		const storyObj = {
+			userTitle : 'The Never Ending Story',
+			photo : 'www.example.com/photo.jpg',
+			userStory : 'Blah blah blah',
+			author : 'John Smith'
+		},
+			handleSubmitEvent = sinon.spy(),
+			dispatch = sinon.spy(),
+			wrapper = shallow(
+				<TextArea
+					props={storyObj}
+					dispatch={dispatch}
+					handleSubmitEvent={handleSubmitEvent} />
+		);
+
+		wrapper.find('button').simulate('submit', function() {
+			handleSubmitEvent.should.have.been.called;
+			dispatch.should.have.been.calledWith(actions.saveStory, actions.updateStory);
+		});
 	});
 });
